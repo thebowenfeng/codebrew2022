c06bb7337ba744860296209132183ce0e3bdab07
thebowenfeng <857514.leofeng@gmail.com>
2022-04-21 13:51:16 +1000

    initialize firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app"
 
 function App() {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCy7Ic92MmSJQ4sSkDLVkeqYm1hzNy39tg",
+    authDomain: "codebrew2022.firebaseapp.com",
+    projectId: "codebrew2022",
+    storageBucket: "codebrew2022.appspot.com",
+    messagingSenderId: "1071289216571",
+    appId: "1:1071289216571:web:8d10c5bbbab9303d74b61e"
+  };
+
+// Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
   return (
     <div className="App">
       <header className="App-header">
