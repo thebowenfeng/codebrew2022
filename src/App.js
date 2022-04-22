90446cd57841b2d191224092402b799d4c2939ab
isaacwu6a25 <isaacwu6a25@gmail.com>
2022-04-22 15:04:38 +1000

    market init

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
-import './App.css';
-import { initializeApp } from "firebase/app"
-import About from "./Pages/About";
-import {useState} from "react";
-import {CurrentPageContext} from "./Contexts/CurrentPageContext";
-import {UserContext} from "./Contexts/UserContext";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { useState } from "react";
+import { CurrentPageContext } from "./Contexts/CurrentPageContext";
+import { UserContext } from "./Contexts/UserContext";
 import GlobalMenubar from "./Components/global_menubar";
 import RoundedButton from "./Components/RoundedButton";
 import RoundedLogoButton from "./Components/RoundedLogoButton";
+
+/* Pages */
+import About from "./Pages/About";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import Market from "./Pages/Market";
 
 function App() {
-  //States
-  const [currentPage, setCurrentPage] = useState("About");
-  const [user, setUser] = useState(null);
+	//States
+	const [currentPage, setCurrentPage] = useState("About");
+	const [user, setUser] = useState(null);
 
-  return (
-    <CurrentPageContext.Provider value={[currentPage, setCurrentPage]}>
-      <UserContext.Provider value={[user, setUser]}>
-        <div className="App">
-          <GlobalMenubar />
-          {currentPage === "About" && <About />}
-          {currentPage === "Login" && <Login/>}
-          {currentPage === "Register" && <Register/>}
-        </div>
-      </UserContext.Provider>
-    </CurrentPageContext.Provider>
-  );
+	return (
+		<CurrentPageContext.Provider value={[currentPage, setCurrentPage]}>
+			<UserContext.Provider value={[user, setUser]}>
+				<div className="App">
+					<GlobalMenubar />
+					{currentPage === "About" && <About />}
+					{currentPage === "Login" && <Login />}
+					{currentPage === "Register" && <Register />}
+					{currentPage === "Market" && <Market />}
+				</div>
+			</UserContext.Provider>
+		</CurrentPageContext.Provider>
+	);
 }
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCy7Ic92MmSJQ4sSkDLVkeqYm1hzNy39tg",
-  authDomain: "codebrew2022.firebaseapp.com",
-  projectId: "codebrew2022",
-  storageBucket: "codebrew2022.appspot.com",
-  messagingSenderId: "1071289216571",
-  appId: "1:1071289216571:web:8d10c5bbbab9303d74b61e"
+	apiKey: "AIzaSyCy7Ic92MmSJQ4sSkDLVkeqYm1hzNy39tg",
+	authDomain: "codebrew2022.firebaseapp.com",
+	projectId: "codebrew2022",
+	storageBucket: "codebrew2022.appspot.com",
+	messagingSenderId: "1071289216571",
+	appId: "1:1071289216571:web:8d10c5bbbab9303d74b61e",
 };
 
 export default App;
---
+++ src/Pages/Market.js
@@ -0,0 +1,10 @@
+
+function Market() {
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    );
+}
+
+export default Market;
